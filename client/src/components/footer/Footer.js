d65b4c72773a1b154562a13a6558ac9062772923
Naoto Tokoyoda <tkyd89@gmail.com>
2023-02-22 14:15:10 -0800

    footer added

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer" >
+          <span className="footerTitleSm">2023 &#169; Naoto Tokoyoda</span>
+    </div>
+  )
+}
+
+export default Footer
