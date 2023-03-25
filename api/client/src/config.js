37f7509825f25e0040bdee7928789e779539eecf
Naoto Tokoyoda <tkyd89@gmail.com>
2023-03-25 15:54:03 -0700

    fixed api

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,22 +1,12 @@
-// import axios from "axios"
+import axios from "axios"
 
-
-// export const axiosInstance = axios.create({
-//     //when you use localhost use this
-//     baseURL: "http://localhost:5001/api/" || "https://naoto-blog.herokuapp.com/api/",
-//     // baseURL: "https://naoto-blog.herokuapp.com/api/",
-//     headers: {
-//         "Content-Type": "application/json", // Set the content type to application/json
-//       },
-// })
-
-import axios from "axios";
-
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: apiUrl,
-  headers: {
-    "Content-Type": "application/json", // Set the content type to application/json
-  },
-});
+    //when you use localhost use this
+    // baseURL: "http://localhost:5001/api/" || "https://naoto-blog.herokuapp.com/api/",
+    baseURL: "https://naoto-blog.herokuapp.com/api/",
+    headers: {
+        "Content-Type": "application/json", // Set the content type to application/json
+      },
+})
+
