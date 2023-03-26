c91ec28ac0179f7ae4769f1416a62ce2c0820ed4
Naoto Tokoyoda <tkyd89@gmail.com>
2023-03-25 17:47:11 -0700

    updated

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -3,8 +3,8 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     //when you use localhost use this
-    baseURL: process.env.REACT_APP_API_URL || process.env.REACT_APP_PROD_API_URL,
-    // baseURL: "https://naoto-blog.herokuapp.com/api/",
+    // baseURL: process.env.REACT_APP_API_URL || process.env.REACT_APP_PROD_API_URL,
+    baseURL: "https://naoto-blog.herokuapp.com/api/",
     headers: {
         "Content-Type": "application/json", // Set the content type to application/json
       },
